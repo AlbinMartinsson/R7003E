fcb42103e5b38ed5a6fd60cf3c8f9c335a717315
AlbinMartinsson <albmar-6@student.ltu.se>
2019-12-11 14:15:37 +0100

    Update LabB.m

--- LabB.m
+++ LabB.m
@@ -40,7 +40,7 @@ Gp = (N*sp) / (D*sp);
 sp2 = [s^4; -s^3; s^2; -s; 1];
 Gm = (N*sp2) / (D*sp2);
 
-rlocus(Gp*Gm);
+%rlocus(Gp*Gm);
 rho = 10;
 afUnsortedAllRoots = rlocus( Gp*Gm, rho );
 [~, aiSortingIndexes] = sort( real(afUnsortedAllRoots) );
@@ -58,18 +58,40 @@ C = [1 0 0 0; 0 0 1 0];
 
 P_o = [P(1) * 4; P(2) * 4 + 0.01; P(3) * 4; P(4) * 4 - 0.01]
 
-L = (place(A', C', P_o))';
+L = (place(A', C', P_o))'
 
 %--------------------------------------------
 %4.8.1 reduced order
 %TODO ... 
 %--------------------------------------------
 
-T_inv = [C ; 0, 1, 0, 0; 0, 0, 0, 1];
+V = [C(2,:);0, 1, 0, 0; 0, 0, 0, 1];
+C_notacc = C(2,:);
+C_acc = C(1,:);
+T_inv = [C(1,:) ; V]
 T = inv(T_inv);
 
-y_acc = C(1,;) * x
-x_hat = T(:,1) * y_acc + T(:,2:)
+A_tilde = T_inv * A * T
+B_tilde = T_inv * B;
+
+
+ C_acc_tilde = C_acc * T;
+ C_notacc_tilde = C_notacc * T;
+
+ n = 4;
+ m = 1;
+ 
+ A_tilde_yy = A_tilde(m:m, m:m)
+% y_acc = C(1,;) * x
+% x_hat = T(:,1) * y_acc + T(:,2:)
+
+
+
+
+
+
+
+
 
 
 
